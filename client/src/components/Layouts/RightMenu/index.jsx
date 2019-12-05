1616fb5000ef2ca93aa4b69e28e2ee4cbbf8e21b
pv <pv@webpractik.ru>
2019-12-06 00:19:36 +0300

    random books

--- client/src/components/Layouts/RightMenu/index.jsx
+++ client/src/components/Layouts/RightMenu/index.jsx
@@ -11,19 +11,16 @@ class RightMenu extends Component {
     };
 
 	componentDidMount() {
-		
+		const arrOfBooks = data.books;
+		const randomBooks = [];
+		for (let i = 0; i <=13; i++) {
+			let index = Math.floor(Math.random() * arrOfBooks.length);
+			randomBooks.push(arrOfBooks[index]);
+		}
+		this.setState({
+			randomBooks: [...new Set(randomBooks)]
+		})
 	}
-
-	componentDidUpdate(prevProps, prevState) {
-        if (prevProps.books) {
-            const arrOfBooks = data.books;
-            const randomBooks = arrOfBooks.filter(book => {
-            	let index = Math.floor(Math.random() * arrOfBooks.length);
-            	return randomBooks.length <= 14 && arrOfBooks[index]
-            });
-	        console.log(randomBooks);
-        }
-    }
 
     render() {
         return (
@@ -32,7 +29,7 @@ class RightMenu extends Component {
                     <div className="rightMenu__title">
                         содержание
                     </div>
-                    {data && data.items.map((item, key) => (
+                    {this.state.randomBooks && this.state.randomBooks.map((item, key) => (
                         <Link to={item.link} className="rightMenu__book" key={item.id}>
                             <span className="rightMenu__book-title">{item.title}</span>
                             <span className="rightMenu__book-num">{key + 1}</span>
