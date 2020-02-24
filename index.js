ec4291726b5d1440ee68b0b96cb95705826996ed
pv <pv@webpractik.ru>
2020-02-24 03:11:25 +0300

    backend

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const path = require('path');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./config/swagger');
+const Sequelize = require('sequelize');
+const {NODE_ENV: env} = process.env;
+const config = require('./config/database')[env];
+
+
+const sequelize = new Sequelize(config);
 // const routes = require('./routes/index');
 
+
+sequelize.authenticate()
+	.then(() => console.log('Db connected...'))
+	.catch(err => console.log('Error: ' + err));
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -18,4 +28,4 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8089);
--- models/Categories.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const categorySchema = new mongoose.Schema({
-	text: {type: String, required: true}
-})
-
-module.exports = mongoose.model('Categories', categorySchema);
